d90a1a745f1b25b95d85af8be8d151a69444695b
Nicklas Utgaard <nutgaard@gmail.com>
2026-05-06 14:37:13 +0200

    noJira: skrevet om til keystorebuilder siden keystoreUtils er deprekert

--- src/main/java/no/statkart/launcher/gradle/plugin/signing/KeystoreSigningExtension.java
+++ src/main/java/no/statkart/launcher/gradle/plugin/signing/KeystoreSigningExtension.java
@@ -1,7 +1,8 @@
 package no.statkart.launcher.gradle.plugin.signing;
 
 import net.jsign.AuthenticodeSigner;
-import net.jsign.KeyStoreUtils;
+import net.jsign.KeyStoreBuilder;
+import net.jsign.KeyStoreType;
 import net.jsign.pe.PEFile;
 
 import java.io.File;
@@ -28,7 +29,11 @@ public class KeystoreSigningExtension implements SigningExtension {
     public void execute(Path into) throws Exception {
         // Støtter bare windows .exe-filer
         if (into.getFileName().toString().endsWith(".exe")) {
-            KeyStore keystore = KeyStoreUtils.load(new File(store), "PKCS12", password, null);
+            KeyStore keystore = new KeyStoreBuilder()
+                .keystore(new File(store))
+                .storetype(KeyStoreType.PKCS12)
+                .storepass(password)
+                .build();
             AuthenticodeSigner signer = new AuthenticodeSigner(keystore, alias, password);
             signer.withTimestamping(false);
             signer.sign(new PEFile(into.toFile()));
